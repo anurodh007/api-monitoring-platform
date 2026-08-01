2902e8f202609a024d2d573733151479984d7cc0
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-01 11:41:16 +0545

    feat(accounts): register user into admin panel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_active']
+
+    list_editable= ['is_active']
