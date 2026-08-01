e33f76cc3b9c31bddd82a75a2e9943eb22665172
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-01 13:16:58 +0545

    feat(monitors): register monitor into admin panel

--- apps/monitors/admin.py
+++ apps/monitors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from monitors.models import Monitor
 
-# Register your models here.
+
+@admin.register(Monitor)
+class MonitorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url', 'method', 'interval', 'timeout', 'expected_status_code', 'is_active']
+
+    list_editable = ['method', 'interval', 'timeout', 'expected_status_code', 'is_active']
+
+    list_filter = ['method']
