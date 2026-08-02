01e948c4c79e9cf2295a05cacf48d8fe4a5c5d40
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-02 19:23:19 +0545

    feat(monitors): limit monitor crud to api-owner only

--- apps/monitors/views.py
+++ apps/monitors/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from monitors.models import Monitor
 from monitors.serializers import MonitorSerializer
-from monitors.permissions import IsOwnerOrReadOnly
+from monitors.permissions import IsOwner
 
 
 class MonitorViewSet(viewsets.ModelViewSet):
@@ -11,9 +11,12 @@ class MonitorViewSet(viewsets.ModelViewSet):
     Viewset to list, create, retrieve, update, delete API Monitors
     """
 
-    queryset = Monitor.objects.select_related('user').order_by('-created_at')
+    queryset = Monitor.objects.select_related('user')
     serializer_class = MonitorSerializer
-    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+    permission_classes = [IsOwner, IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
