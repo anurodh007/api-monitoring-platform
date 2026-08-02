faf355c791a7078df7304b41a9e02c68050353af
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-02 15:14:22 +0545

    feat(monitoring): register monitoring-result model into admin panel

--- apps/monitoring/admin.py
+++ apps/monitoring/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from monitoring.models import MonitoringResult
 
-# Register your models here.
+
+@admin.register(MonitoringResult)
+class MonitoringResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'monitor', 'status_code', 'response_time', 'is_successful', 'error_message', 'checked_at']
+
+    list_filter = ['is_successful']
