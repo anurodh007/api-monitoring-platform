87266036cd03d9b268f054aa7b87daf99cf2b6e7
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-05 15:26:37 +0545

    feat(incidents): register incident model into admin panel

--- apps/incidents/admin.py
+++ apps/incidents/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from incidents.models import Incident
 
-# Register your models here.
+
+@admin.register(Incident)
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'severity', 'failure_count', 'last_failed_at']
