6fe1d3ee352973040bbbe637d2cdad9faa1774e8
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-08-05 17:27:53 +0545

    feat(incidents): filter incidents by status and severity

--- apps/incidents/views.py
+++ apps/incidents/views.py
@@ -7,6 +7,7 @@ from incidents.serializers import IncidentSerializer
 class IncidentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Incident.objects.select_related('monitor__user')
     serializer_class = IncidentSerializer
+    filterset_fields = ['status', 'severity']
 
     def get_queryset(self):
         return self.queryset.filter(monitor__user_id=self.request.user.id).order_by('-started_at')
